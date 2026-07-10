57bc41a8499cdf57aedfb9e670093a1417a230c8
DJ Thornton <thornton_dj@hotmail.com>
2026-07-10 18:39:41 -0500

    feat: :sparkles: podman install for 5.x on noble

--- apps/install.sh
+++ apps/install.sh
@@ -1,74 +1,116 @@
 #! /bin/bash
+# Ignore crlf when coding on windows for linux, I know the irony...
+# shellcheck disable=SC1017
+
+function aider {
+	echo "Installing Aider ..."
+
+	python -m pip install pipx  # If you need to install pipx
+	pipx install aider-install setuptools
+	aider-install
+}
+
+# Install Podman 5.3.0 on Kubuntu 24.04 LTS (Noble Numbat)
+# We must use APT pinning to pull the package from the newer Ubuntu 25.04
+# (Plucky) repository, as the official Noble repository only contains version
+# 4.9.3.
+function podman {
+	echo "Installing Podman ..."
+
+	function podman_apt_pinning {
+		sudo mkdir -p /etc/apt/sources.list.d
+		echo "deb http://archive.ubuntu.com/ubuntu plucky main universe" |
+			sudo tee /etc/apt/sources.list.d/plucky.list
+
+		sudo tee /etc/apt/preferences.d/podman-plucky.pref > /dev/null <<- EOF
+		Package: podman buildah golang-github-containers-common crun libgpgme11t64 libgpg-error0 golang-github-containers-image catatonit conmon containers-storage
+		Pin: release n=plucky
+		Pin-Priority: 991
+
+		Package: libsubid4 netavark passt aardvark-dns containernetworking-plugins libslirp0 slirp4netns
+		Pin: release n=plucky
+		Pin-Priority: 991
+
+		Package: *
+		Pin: release n=plucky
+		Pin-Priority: 400
+		EOF
+	}
+
+	echo "Pinning plucky apt source for podman 5.x"
+	podman_apt_pinning
+
+	echo "Install Podman and some dependencies"
+	apt-get update && apt-get install -y \
+		ca-certificates curl gnupg lsb-release software-properties-common
+
+	echo "Install Podman and some dependencies"
+	apt-get install -y podman.io
+
+	echo "Podman installed successfully!"
+}
 
 # Install Spotify through Debian
 # Source: https://www.spotify.com/us/download/linux/
-
-function aider {
-  echo "Installing Aider ..."
-
-  python -m pip install pipx  # If you need to install pipx
-  pipx install aider-install setuptools
-  aider-install
-}
-
 function spotify {
-  echo "Installing Spotify dependencies..."
+	echo "Installing Spotify dependencies..."
 
-  curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc |
-    sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+	curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc |
+		sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 
-  echo "deb https://repository.spotify.com stable non-free" |
-    sudo tee /etc/apt/sources.list.d/spotify.list
+	echo "deb https://repository.spotify.com stable non-free" |
+		sudo tee /etc/apt/sources.list.d/spotify.list
 
-  sudo apt-get update
-  sudo apt-get install -y spotify-client
+	sudo apt-get update
+	sudo apt-get install -y spotify-client
 }
 
 # Install Steam on Ubuntu with NVIDIA drivers
 # Source: https://linuxcapable.com/how-to-install-steam-on-ubuntu-linux/
 function steam {
-  echo "Installing Steam dependencies..."
-  # Update package lists, upgrade existing packages, and install necessary dependencies for Steam
-  sudo apt update
-  # Upgrade existing packages to ensure compatibility and security
-  sudo apt upgrade -y
-  # Install essential dependencies for Steam, including support for 32-bit architecture and tools for managing software repositories
-  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-  # Add support for 32-bit architecture, which is required for running many Steam games that are not available in 64-bit versions
-  sudo dpkg --add-architecture i386
-  sudo apt update
+	echo "Installing Steam dependencies..."
+	# Update package lists, upgrade existing packages, and install necessary dependencies for Steam
+	sudo apt-get update
+	# Upgrade existing packages to ensure compatibility and security
+	sudo apt-get upgrade -y
+	# Install essential dependencies for Steam, including support for 32-bit architecture and tools for managing software repositories
+	sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+	# Add support for 32-bit architecture, which is required for running many Steam games that are not available in 64-bit versions
+	sudo dpkg --add-architecture i386
+	sudo apt-get update
 
-  echo "Installing Steam from the official Ubuntu repository..."
+	echo "Installing Steam from the official Ubuntu repository..."
 
-  # Download and add the Steam GPG key to the system's keyring
-  sudo curl -fsSLo /usr/share/keyrings/steam.gpg https://repo.steampowered.com/steam/archive/stable/steam.gpg
-  sudo chmod 0644 /usr/share/keyrings/steam.gpg
+	# Download and add the Steam GPG key to the system's keyring
+	sudo curl -fsSLo /usr/share/keyrings/steam.gpg https://repo.steampowered.com/steam/archive/stable/steam.gpg
+	sudo chmod 0644 /usr/share/keyrings/steam.gpg
 
-  # Add the Steam repository to the system's sources list
-  printf '%s\n' \
-  'Types: deb' \
-  'URIs: https://repo.steampowered.com/steam/' \
-  'Suites: stable' \
-  'Components: steam' \
-  'Architectures: amd64 i386' \
-  'Signed-By: /usr/share/keyrings/steam.gpg' |
-    sudo tee /etc/apt/sources.list.d/steam.sources >/dev/null
+	# Add the Steam repository to the system's sources list
+	printf '%s\n' \
+	'Types: deb' \
+	'URIs: https://repo.steampowered.com/steam/' \
+	'Suites: stable' \
+	'Components: steam' \
+	'Architectures: amd64 i386' \
+	'Signed-By: /usr/share/keyrings/steam.gpg' |
+		sudo tee /etc/apt/sources.list.d/steam.sources >/dev/null
 
-  # Refresh package lists to include the Steam repository
-  sudo apt update
+	# Refresh package lists to include the Steam repository
+	sudo apt-get update
 
-  # Install Valve's official Steam package
-  sudo apt install -y steam-launcher
+	# Install Valve's official Steam package
+	sudo apt-get install -y steam-launcher
 
-  if [[ -f "/etc/apt/sources.list.d/steam-stable.list" ]]; then
-    echo "Removing old (duplicate) Steam repository source list..."
-    sudo rm -f /etc/apt/sources.list.d/steam-stable.list
-  fi
+	if [[ -f "/etc/apt/sources.list.d/steam-stable.list" ]]; then
+		echo "Removing old (duplicate) Steam repository source list..."
+		sudo rm -f /etc/apt/sources.list.d/steam-stable.list
+	fi
 
-  # Verify that Steam was installed successfully by checking the package list
-  dpkg -l | grep steam
+	# Verify that Steam was installed successfully by checking the package list
+	dpkg -l | grep steam
 }
 
 aider
+podman
 steam
 spotify
